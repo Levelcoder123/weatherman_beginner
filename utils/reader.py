a8a8c2b3f8702a21a1b7e2c5f823cc36c0f88ccf
Levelcoder123 <muaz.abdulwahid@barq.dev>
2023-11-09 19:55:53 +0500

    more changes in files

--- utils/reader.py
+++ utils/reader.py
@@ -4,22 +4,21 @@ import os
 class WeatherDataReader:
     all_years_data = []
 
-    def __init__(self, _folder_path):
-        self._folder_path = _folder_path
-        self._all_items = os.listdir(self._folder_path)
-        self._files_path = [os.path.join(self._folder_path, item) for item in self._all_items]
+    folder_path = "../weather_files"
+    all_items = os.listdir(folder_path)
+    files_path = [os.path.join(folder_path, item) for item in all_items]
 
     # iterate through the paths and extract all data.
-    def path_finder(self):
-        for path in self._files_path:
+    @classmethod
+    def path_finder(cls):
+        for path in WeatherDataReader.files_path:
             with open(path, mode='r') as file:
                 file_read = file.readlines()
                 for line in file_read[1:]:
                     data = line.strip('\n').split(',')
-                    self.all_years_data.append(data)
+                    WeatherDataReader.all_years_data.append(data)
 
-        return self.all_years_data
+        return WeatherDataReader.all_years_data
 
 
-folder_path = "../weather_files"
-weather_object = WeatherDataReader(folder_path)
+weather_object = WeatherDataReader.path_finder()
